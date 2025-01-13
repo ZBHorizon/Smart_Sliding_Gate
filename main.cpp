f46a2d7d3ed03a2715d0aee3df35a692562f3ea0
Noah Meyer <nmstorbeck@gmail.com>
2025-01-13 20:24:22 +0100

    Projektdateien hinzufügen.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <wiringPi.h>
+
+// "LED-Pin: wiringPi-Pin 0" entspricht "BCM_GPIO 17".
+// Bei einer Initialisierung mit wiringPiSetupSys muss die BCM-Nummerierung verwendet werden.
+// Wenn Sie eine andere Pin-Nummer wählen, verwenden Sie die BCM-Nummerierung, und
+// aktualisieren Sie außerdem unter "Eigenschaftenseiten" > "Buildereignisse" den Remote-Postbuildereignisbefehl,
+// der den GPIO-Export zum Einrichten von wiringPiSetupSys verwendet.
+#define	LED	17
+
+int main(void)
+{
+	wiringPiSetupSys();
+
+	pinMode(LED, OUTPUT);
+
+	while (true)
+	{
+		digitalWrite(LED, HIGH);  // Ein
+		delay(500); // ms
+		digitalWrite(LED, LOW);	  // Aus
+		delay(500);
+	}
+	return 0;
+}
