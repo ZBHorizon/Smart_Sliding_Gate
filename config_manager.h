3a112e356e9e453ab2f46028d278278bd0ceb80a
Noah Meyer <nmstorbeck@gmail.com>
2025-01-15 21:19:14 +0100

    removed makros

--- config_manager.h
+++ config_manager.h
@@ -1,14 +1,11 @@
-#ifndef CONFIG_MANAGER_H
-#define CONFIG_MANAGER_H
-
+#pragma once
 #include <string>
 #include <cstdint>
 
 
-//brief Appends a named variable and its value to a binary file.
+//Appends a named variable and its value to a binary file.
 void save_variable(const std::string& var_name, std::int32_t value);
 
-//brief Reads the value of a specific variable from the binary file.
+//Reads the value of a specific variable from the binary file.
 std::int32_t load_variable(const std::string& var_name);
 
-#endif 
